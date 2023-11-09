9a18ae7b590b575513b2e294424e1621ca0b9c3c
KhimFranco <khimangelofranco@gmail.com>
2023-11-09 16:46:35 +0800

    feat insert data to owners

--- veterinary.sql
+++ veterinary.sql
@@ -62,3 +62,16 @@ PRIMARY KEY (record_id),
 FOREIGN KEY (animal_id) REFERENCES animals(animal_id)
 FOREIGN KEY (doctor_id) REFERENCES doctors(doctor_id)
 );
+
+ INSERT INTO owners (ownerid, ofirstname, olastname, address, phone, email)
+VALUES
+    (1, 'John', 'Doe', '123 Main St', '555-555-1212', 'johndoe@example.com'),
+    (2, 'Jane', 'Doe', '456 Elm St', '555-555-1212', 'janedoe@example.com'),
+    (3, 'Bob', 'Smith', '789 Oak St', '555-555-1212', 'bobsmith@example.com'),
+    (4, 'Sue', 'Jones', '321 Pine St', '555-555-1212', 'suejones@example.com'),
+    (5, 'Tom', 'Brown', '654 Maple St', '555-555-1212', 'tombrown@example.com'),
+    (6, 'Mary', 'Green', '987 Cedar St', '555-555-1212', 'marygreen@example.com'),
+    (7, 'Bill', 'White', '246 Birch St', '555-555-1212', 'billwhite@example.com'),
+    (8, 'Amy', 'Black', '135 Walnut St', '555-555-1212', 'amyblack@example.com'),
+    (9, 'Mike', 'Gray', '864 Cherry St', '555-555-1212', 'mikegray@example.com'),
+    (10, 'Lisa', 'Taylor', '975 Spruce St', '555-555-1212', 'lisataylor@example.com');

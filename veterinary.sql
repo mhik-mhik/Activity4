7fbd6920bce5f5dafd583248f6f2d56df3d22fa5
SusanJraOmbao08 <susanjraombao08@gmail.com>
2023-11-09 16:21:21 +0800

    feat/create-table-animals

--- veterinary.sql
+++ veterinary.sql
@@ -8,3 +8,17 @@ CREATE TABLE owners (
     phone VARCHAR(15)
     email VARCHAR(100)
 );
+
+CREATE TABLE IF NOT EXISTS public.animals
+(
+animal_id INT NOT NULL,
+name VARCHAR(50) NOT NULL,
+species VARCHAR(50) NOT NULL,
+breed VARCHAR(50) NOT NULL,
+date_of_birth DATE NOT NULL,
+gender VARCHAR(10) NOT NULL,
+color VARCHAR(50) NOTY NULL,
+owner_id INT NOT NULL,
+PRIMARY KEY(animalid),
+FOREIGN KEY (owner_id) REFERENCES owners(owner_id)
+);

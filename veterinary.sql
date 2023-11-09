c6f2ba3820092d9f34d184d07a1c56a004a297be
KhimFranco <khimangelofranco@gmail.com>
2023-11-09 17:35:50 +0800

    feat/remove-appoinment-simba

--- veterinary.sql
+++ veterinary.sql
@@ -140,3 +140,11 @@ ADD COLUMN registereddate DATE;
 
 ALTER TABLE invoices
 RENAME COLUMN paymentdate TO paymenttime;
+
+DELETE FROM invoices
+WHERE appointid = (
+	SELECT appointid 
+	FROM appointments 
+	WHERE animalid = (
+		SELECT animalid 
+		FROM animals WHERE name = 'Simba'));
